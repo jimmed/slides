49930935e558a750a760478ff625e735b1522ed0
Jim O'Brien <jim@jimmed.net>
2020-07-04 13:19:03 +0100

    Add tsconfig to make linter shut up

--- components/Diagram.js
+++ components/Diagram.js
@@ -4,5 +4,14 @@ import { useSteps } from "mdx-deck";
 export const Diagram = ({ images, alt }) => {
   const index = useSteps(images.length - 1);
 
-  return <object data={images[index]}>{alt}</object>;
+  return (
+    <object data={images[index]}>
+      {alt}{" "}
+      {images.length > 1 && (
+        <>
+          {index + 1} / {images.length}
+        </>
+      )}
+    </object>
+  );
 };
